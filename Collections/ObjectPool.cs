64e6156c48ef6e2b4211f0be3da2e4fd90aaa2d3
TheDustySheep <thericoboy22@gmail.com>
2025-05-11 19:27:05 +1000

    Implemented Voxel Sky and Torch Lighting

--- Collections/ObjectPool.cs
+++ Collections/ObjectPool.cs
@@ -24,6 +24,31 @@ namespace FainEngine_v2.Collections
         }
     }
 
+    public class LimitedObjectPool<T> where T : new()
+    {
+        private readonly int _maxItems;
+        public readonly ConcurrentBag<T> Bag = new();
+        public LimitedObjectPool(int count)
+        {
+            _maxItems = count;
+            for (int i = 0; i < count; i++)
+                Bag.Add(new T());
+        }
+
+        public bool TryRequest(out T item)
+        {
+            return Bag.TryTake(out item!);
+        }
+
+        public void Return(T item)
+        {
+            if (Bag.Count >= _maxItems)
+                return;
+
+            Bag.Add(item);
+        }
+    }
+
     public class ObjectPoolFactory<T>
     {
         public int MaxItems { get; set; } = int.MaxValue;
---
+++ Extensions/ListExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FainEngine_v2.Extensions
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Sorts the list based on a key selector that returns a numeric value.
+        /// </summary>
+        public static void SortBy<T, TKey>(this List<T> list, Func<T, TKey> keySelector)
+            where TKey : IComparable<TKey>
+        {
+            list.Sort((a, b) => keySelector(a).CompareTo(keySelector(b)));
+        }
+
+        /// <summary>
+        /// Sorts the list based on a key selector in descending order.
+        /// </summary>
+        public static void SortByDescending<T, TKey>(this List<T> list, Func<T, TKey> keySelector)
+            where TKey : IComparable<TKey>
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+            if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+
+            list.Sort((a, b) => keySelector(b).CompareTo(keySelector(a)));
+        }
+
+        /// <summary>
+        /// Tries to dequeue (remove and return) the first element of the list.
+        /// </summary>
+        /// <typeparam name="T">Type of the list elements</typeparam>
+        /// <param name="list">The list instance</param>
+        /// <param name="item">The dequeued item if successful</param>
+        /// <returns>True if an item was dequeued, false if the list is empty</returns>
+        public static bool TryDequeueFirst<T>(this List<T> list, out T item)
+        {
+            if (list.Count > 0)
+            {
+                item = list[0];
+                list.RemoveAt(0);
+                return true;
+            }
+
+            item = default!;
+            return false;
+        }
+
+        /// <summary>
+        /// Tries to dequeue (remove and return) the last element of the list.
+        /// </summary>
+        /// <typeparam name="T">Type of the list elements</typeparam>
+        /// <param name="list">The list instance</param>
+        /// <param name="item">The dequeued item if successful</param>
+        /// <returns>True if an item was dequeued, false if the list is empty</returns>
+        public static bool TryDequeueLast<T>(this List<T> list, out T item)
+        {
+            int lastIndex = list.Count - 1;
+            if (lastIndex >= 0)
+            {
+                item = list[lastIndex];
+                list.RemoveAt(lastIndex);
+                return true;
+            }
+
+            item = default!;
+            return false;
+        }
+    }
+}
